ce6200710c348ce47489220292249c02352f11cb
Howy Perrin <howyp@users.noreply.github.com>
2018-11-16 17:29:22 +0000

    Add arbitrary instance and round-trip tests for `AnyString`

--- scalacheck/src/main/scala/ropes/scalacheck/scalacheck.scala
+++ scalacheck/src/main/scala/ropes/scalacheck/scalacheck.scala
@@ -5,4 +5,6 @@ import org.scalacheck.{Arbitrary, Gen}
 package object scalacheck {
   implicit def arbExactlyChar[C <: Char with Singleton](implicit c: ValueOf[C]): Arbitrary[Exactly[C]] =
     Arbitrary(Gen.const(Exactly(c.value)))
+
+  implicit val arbAnyString: Arbitrary[AnyString] = Arbitrary(Arbitrary.arbitrary[String].map(AnyString.apply))
 }
--- tests/src/test/scala/unit/AnyStringSpec.scala
+++ tests/src/test/scala/unit/AnyStringSpec.scala
@@ -1,8 +1,10 @@
 package unit
 
+import org.scalacheck.Arbitrary
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalatest.{FreeSpec, Matchers}
 import ropes._
+import ropes.scalacheck._
 
 class AnyStringSpec extends FreeSpec with Matchers with GeneratorDrivenPropertyChecks {
   "An `AnyString` Rope" - {
@@ -12,6 +14,15 @@ class AnyStringSpec extends FreeSpec with Matchers with GeneratorDrivenPropertyC
     "Always writes back to the same string" in forAll { s: String =>
       AnyString(s).write should be(s)
     }
-    //TODO Generate instance for String
+    "Round-trips valid strings by parsing and writing back to an identical string" in forAll(
+      Arbitrary.arbitrary[String]) { original =>
+      val Parse.Result.Complete(parsed) = Rope.parseTo[AnyString](original)
+      parsed.write should be(original)
+    }
+    "Round-trips arbitrary values by writing and parsing back to an identical value" in forAll(
+      Arbitrary.arbitrary[AnyString]) { original =>
+      val Parse.Result.Complete(parsed) = Rope.parseTo[AnyString](original.write)
+      parsed should be(original)
+    }
   }
 }

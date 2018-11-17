45dd11b14a00cf78ec7d9df12ee5850183f9e22a
Howy Perrin <howyp@users.noreply.github.com>
2018-11-17 19:19:59 +0000

    Add test for valid example of twitter handle #6

--- tests/src/test/scala/integration/ExamplesSpec.scala
+++ tests/src/test/scala/integration/ExamplesSpec.scala
@@ -2,11 +2,15 @@ package integration
 
 import org.scalatest.{FreeSpec, Matchers}
 
+import ropes._
+
 class ExamplesSpec extends FreeSpec with Matchers {
   "Some examples of valid ropes include" - {
+    //This is very simplified - starts with an '@', then any characters
+    type TwitterHandle = Concat[Exactly['@'], AnyString]
     "twitter handles" in {
-      pending
-      //Starts with an '@', then any characters
+      val Parse.Result.Complete(parsed) = Rope.parseTo[TwitterHandle]("@howyp")
+      parsed.suffix.value should be("howyp")
     }
   }
 }

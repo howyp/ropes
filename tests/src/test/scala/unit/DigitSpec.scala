968448becd04b6dfc86cd1eaae3798a854a4dfd4
Howy Perrin <howyp@users.noreply.github.com>
2018-11-26 19:12:19 +0000

    #14 Test `Digit` for incomplete and invalid strings

--- tests/src/test/scala/unit/DigitSpec.scala
+++ tests/src/test/scala/unit/DigitSpec.scala
@@ -1,17 +1,25 @@
 package unit
+import gens.CommonGens
 import laws.RopeLaws
 import org.scalacheck.Gen
 import ropes._
 import ropes.scalacheck._
 
-class DigitSpec extends RopeLaws {
+class DigitSpec extends RopeLaws with CommonGens {
   "A Digit" - {
     `obeys Rope laws`[Digit](
       genValidStringsWithDecompositionAssertion = Gen.choose(0, 9).map { digit =>
         digit.toString -> (_.value should be(digit))
       },
-      genSuffixToValidStringIncomplete = None,
-      genInvalidStrings = None
+      genSuffixToValidStringIncomplete = Some(genNonEmptyString),
+      genInvalidStrings = Some(
+        Gen
+          .oneOf(
+            Gen.choose(Char.MinValue, ('0' - 1).toChar),
+            Gen.choose(('9' + 1).toChar, Char.MaxValue)
+          )
+          .map(_.toString)
+      )
     )
   }
 }

0fdabad2d50f4ede9e68d5fa2c8d5579d6722a79
Howy Perrin <howyp@users.noreply.github.com>
2018-11-26 11:53:12 +0000

    #9 Add explicit tests for `Range.from`

--- tests/src/test/scala/unit/RangeSpec.scala
+++ tests/src/test/scala/unit/RangeSpec.scala
@@ -8,19 +8,25 @@ import ropes.scalacheck._
 
 class RangeSpec extends RopeLaws with CommonGens {
   "A Range['a','z']" - {
+    val genAtoZ = Gen.choose('a', 'z')
+    val genNonAtoZ = Gen
+      .oneOf(
+        Gen.choose(Char.MinValue, ('a' - 1).toChar),
+        Gen.choose(('z' + 1).toChar, Char.MaxValue)
+      )
+
     `obeys Rope laws`[Range['a', 'z']](
-      genValidStringsWithDecompositionAssertion = Gen.choose('a', 'z').map { char =>
+      genValidStringsWithDecompositionAssertion = genAtoZ.map { char =>
         char.toString -> (_.value should be(char))
       },
       genSuffixToValidStringIncomplete = Some(genNonEmptyString),
-      genInvalidStrings = Some(
-        Gen
-          .oneOf(
-            Gen.choose(Char.MinValue, ('a' - 1).toChar),
-            Gen.choose(('z' + 1).toChar, Char.MaxValue)
-          )
-          .map(_.toString)
-      )
+      genInvalidStrings = Some(genNonAtoZ.map(_.toString))
     )
+    "Range can be created from valid characters" - forAll(genAtoZ) { char =>
+      Range.from['a', 'z'](char).get should have('value (char))
+    }
+    "Range cannot be created from invalid characters" - forAll(genNonAtoZ) { char =>
+      Range.from['a', 'z'](char) should be(empty)
+    }
   }
 }
